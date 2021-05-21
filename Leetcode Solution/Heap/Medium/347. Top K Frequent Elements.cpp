199c79b819fe984d1230ef205deb0c7dfb3ddbc6
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-21 16:08:03 +0530

    Create 347. Top K Frequent Elements.cpp

---
+++ Leetcode Solution/Heap/Medium/347. Top K Frequent Elements.cpp
@@ -0,0 +1,32 @@
+
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+            unordered_map<int,int> mp;
+            vector<int> ans;
+            for(int i=0;i<nums.size();i++)
+            {
+                  mp[nums[i]]++;  
+            }
+            for(auto i: mp)
+            {
+                    pq.push({i.second,i.first});
+                    
+                    if(pq.size()>k)
+                    {
+                            pq.pop();
+                    }
+            }
+            while(pq.size())
+            {
+                    ans.push_back(pq.top().second);
+                    pq.pop();
+            }
+            
+            return ans;
+            
+    }
+};
