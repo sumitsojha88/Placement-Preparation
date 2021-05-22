eeb29f074b981ddbc9e354b8d66816c284ec8129
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-22 22:59:33 +0530

    Create 589. N-ary Tree Preorder Traversal.cpp

---
+++ Leetcode Solution/Tree/Easy/589. N-ary Tree Preorder Traversal.cpp
@@ -0,0 +1,41 @@
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        if(root==nullptr)
+                return{};
+        stack<Node*> st;
+        vector<int> ans;
+        st.push(root);
+        
+        while(!st.empty()){
+            Node* node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            for(int i=(node->children).size()-1;i>=0;--i){
+                st.push(node->children[i]);
+            }
+        }
+        return ans;
+    }
+};
