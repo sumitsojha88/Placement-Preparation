c2ce2d6ba08ab6a4519a874d717188d40111add5
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-23 00:18:17 +0530

    Create 173. Binary Search Tree Iterator.cpp

---
+++ Leetcode Solution/Tree/Medium/173. Binary Search Tree Iterator.cpp
@@ -0,0 +1,35 @@
+
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+
+
+class BSTIterator {
+    stack<TreeNode *> myStack;
+public:
+    BSTIterator(TreeNode *root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *tmpNode = myStack.top();
+        myStack.pop();
+        pushAll(tmpNode->right);
+        return tmpNode->val;
+    }
+
+private:
+    void pushAll(TreeNode *node) {
+        for (; node != NULL; myStack.push(node), node = node->left);
+    }
+};
