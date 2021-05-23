682600142fc434842fe4a32a21cd915f596536c7
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-23 21:09:59 +0530

    Create 1870. Minimum Speed to Arrive on Time.cpp

---
+++ Leetcode Solution/Particular Patterns Question/Binary Search Patterns/1870. Minimum Speed to Arrive on Time.cpp
@@ -0,0 +1,43 @@
+
+
+class Solution {
+public:
+    int minSpeedOnTime(vector<int>& d, double hour) {
+            
+            int n=d.size();
+            if(n>ceil(hour))
+            {
+                    return -1;
+            }
+            
+        //    int n=d.size();
+            int l=1;
+            int r=1e7;
+            int ans=-1;
+            while(l<=r)
+            {
+                    int m = (l+r)/2;
+                    double sum = 0;
+                    for(int i=0;i<n-1;i++)
+                    {
+                            sum +=  ceil((double)d[i]/m);
+                    }
+                    sum += (double)(d[n-1])/m;
+                    
+                    if(sum>hour)
+                    {
+                            
+                            l = m + 1;
+                    }
+                    else
+                    {
+                            ans = m;
+                            r  = m-1;
+                    }
+            }
+            
+            return ans;
+            
+        
+    }
+};
