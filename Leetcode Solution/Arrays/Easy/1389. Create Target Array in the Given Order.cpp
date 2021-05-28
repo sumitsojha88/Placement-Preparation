050127e62158977300c9a3dce4f2d3e18e222676
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-28 20:46:42 +0530

    Create 1389. Create Target Array in the Given Order.cpp

---
+++ Leetcode Solution/Arrays/Easy/1389. Create Target Array in the Given Order.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+            vector<int> ans;
+            
+            for(int i=0;i<nums.size();i++)
+            {
+                    ans.insert(ans.begin()+index[i],nums[i]);
+            }
+        
+            return ans;
+    }
+};
