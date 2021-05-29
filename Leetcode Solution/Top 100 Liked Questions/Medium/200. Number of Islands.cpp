0518e94aefc3dff8d9fa55a36490f54b97e46a8c
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-29 22:34:25 +0530

    Create 200. Number of Islands.cpp

---
+++ Leetcode Solution/Top 100 Liked Questions/Medium/200. Number of Islands.cpp
@@ -0,0 +1,42 @@
+
+class Solution {
+public:
+        void dfs(vector<vector<char>>& grid, vector<vector<bool>>& v, int i, int j, int n, int m)
+        {
+           if(i<0 || i>=n || j<0 || j>=m )
+           {
+                   return ;
+           }
+                
+           if(grid[i][j]=='0' || v[i][j]) return;
+                
+                v[i][j]=true;
+                
+                dfs(grid,v,i+1,j,n,m);
+                dfs(grid,v,i,j-1,n,m);
+                dfs(grid,v,i-1,j,n,m);
+                dfs(grid,v,i,j+1,n,m);
+        }
+    int numIslands(vector<vector<char>>& grid) {
+            if (grid.empty()) return 0;
+
+            int n=grid.size();
+            int m=grid[0].size();
+            vector<vector<bool>> v(n,vector<bool>(m,false));
+            int ans=0;
+            for(int i=0;i<n;i++)
+            {
+                    for(int j=0;j<m;j++)
+                    {
+                         if(grid[i][j]=='1' && !v[i][j])  
+                         {
+                                 dfs(grid,v,i,j,n,m);
+                                 ans++;
+                         }
+                    }
+            }
+            
+            return ans;
+        
+    }
+};
