069f1e4e18453aa41803e08ff172f03bfd79c612
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-29 22:32:05 +0530

    Create 739. Daily Temperatures.cpp

---
+++ Leetcode Solution/Top 100 Liked Questions/Medium/739. Daily Temperatures.cpp
@@ -0,0 +1,42 @@
+
+lass Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+            
+            
+            
+            int n=T.size();
+            vector<int> res;
+            stack<int> s;
+          
+            for(int i=n-1;i>=0;i--)
+            {
+                          while( !s.empty() && T[s.top()]<=T[i] )
+                            {
+                                    s.pop();
+                            }
+                    
+                    
+                            if(s.empty())
+                            {
+                                   res.push_back(0);
+                            }
+                            else
+                            {
+                               res.push_back(s.top()-i);
+                            }
+                    
+                    
+                    
+                    s.push(i);
+            }
+            
+            reverse(res.begin(),res.end());
+            
+            return res; 
+            
+            
+     
+        
+    }
+};
