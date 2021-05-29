cbfdc972f2c80d09e7e4dceda8c9a1db3023ae21
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-29 22:30:03 +0530

    Create 20. Valid Parentheses.cpp

---
+++ Leetcode Solution/Top 100 Liked Questions/Easy/20. Valid Parentheses.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        
+            stack<char> st;
+            for(int i=0;i<s.size();i++)
+            {
+                    if(st.empty())
+                    {
+                            st.push(s[i]);
+                    }
+                    else if(s[i]=='(' || s[i]=='{' || s[i]=='[' )
+                    {
+                         st.push(s[i]);   
+                    }
+                    else 
+                    {
+                            if(s[i]=='}' && st.top()!='{')
+                                    return false;
+                          else  if(s[i]==')' && st.top()!='(')
+                                   return false;
+                          else  if(s[i]==']' && st.top()!='[')
+                                    return false;
+                            
+                            
+                            st.pop();
+
+                    }
+            }
+            
+            
+            
+            
+            if(st.empty())
+            return true;
+            else
+            return false;
+    }
+};
