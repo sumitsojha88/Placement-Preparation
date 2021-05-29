305d5cadfffde4a30eb72186480f365d36171b5f
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-05-29 22:32:43 +0530

    Create 49. Group Anagrams.cpp

---
+++ Leetcode Solution/Top 100 Liked Questions/Medium/49. Group Anagrams.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& s) {
+        
+            unordered_map<string,vector<string>> mp;
+           
+            for(int i=0;i<s.size();i++)
+            {
+                   string t = s[i];
+                    sort(t.begin(),t.end());
+                    mp[t].push_back(s[i]);
+            }
+            int i=0;
+             vector<vector<string>> ans;
+            ans.resize(mp.size(),{});
+            
+            for(auto m :mp)
+            {
+                    for(auto j:m.second)
+                    {
+                         ans[i].push_back(j);   
+                    }
+                    i++;
+            }
+            
+            return ans;
+            
+    }
+};
