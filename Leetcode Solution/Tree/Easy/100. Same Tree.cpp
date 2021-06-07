24a1e023bccfb836035f504e0624181d32722358
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-06-07 12:09:38 +0530

    Update 100. Same Tree.cpp

--- Leetcode Solution/Tree/Easy/100. Same Tree.cpp
+++ Leetcode Solution/Tree/Easy/100. Same Tree.cpp
@@ -9,22 +9,30 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+Solution 1: // Time O(n) space O(n)
+Approach :
+1)find inorder traversal of both
+2) match them if they are equal means tree is same else not.
+
+
+
+Solution 2:
+// Time O(n) space O(1)
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
-          
-            if(p==NULL && q==NULL)
-                    return true;
-             if(p==NULL || q==NULL)
-                    return false;
-            if(p->val == q->val)
-            {
-                return isSameTree(p->left,q->left) && isSameTree(p->right,q->right) ;   
-            }
-             else
-            return false; 
             
-           
+            if(p==NULL && q==NULL) return true;
+            if(p==NULL && q!=NULL) return false;
+            if(p!=NULL && q==NULL) return false;
+            
+            if(p->val!=q->val)
+            {
+                    return false;
+            }
+            
+            
+            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        
     }
 };
