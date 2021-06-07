25daf4601fcc4695adc08465d410da47f26f42a7
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-06-07 14:03:29 +0530

    Update 108. Convert Sorted Array to Binary Search Tree.cpp

--- Leetcode Solution/Tree/Easy/108. Convert Sorted Array to Binary Search Tree.cpp
+++ Leetcode Solution/Tree/Easy/108. Convert Sorted Array to Binary Search Tree.cpp
@@ -5,31 +5,40 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return sortedArrayToBST(nums,0,nums.size()-1);
-    }
-        TreeNode* sortedArrayToBST(vector<int>& nums,int l,int r)
+        TreeNode* maketree(vector<int>& nums,int l,int h)
         {
-                
-          if(l>r)
-                  return 0;
-                if(l==r)
+                if(l>h)
+                {
+                        return 0;
+                }
+                if(l==h)
+                {
                         return new TreeNode(nums[l]);
-                    int mid=(l+r)/2 ;
+                }
                 
-                TreeNode* root=new TreeNode(nums[mid]);
-                root->left =sortedArrayToBST(nums,l,mid-1);
-                root->right=sortedArrayToBST(nums,mid+1,r);
+                int mid = (l+h)/2;
+                         
+                
+                
+                TreeNode* root = new TreeNode(nums[mid]);
+                root->left = maketree(nums,l,mid-1);
+                root->right = maketree(nums,mid+1,h);
                 
                 
                 return root;
-        
-        
-        
         }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+            
+            int n=nums.size();
+            return maketree(nums,0,n-1);
+            
+        
+    }
 };
