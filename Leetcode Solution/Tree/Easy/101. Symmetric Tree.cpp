21dedd45f9713c90b1d6f58bfe30d064f5dbdac6
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-06-07 12:15:25 +0530

    Update 101. Symmetric Tree.cpp

--- Leetcode Solution/Tree/Easy/101. Symmetric Tree.cpp
+++ Leetcode Solution/Tree/Easy/101. Symmetric Tree.cpp
@@ -1,35 +1,37 @@
 
+Almost same as Question 100. Same Tree:- https://leetcode.com/problems/same-tree/
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Time O(N) Space O(1)
 class Solution {
 public:
- bool isMirror(TreeNode* root1,TreeNode* root2)
-   {
-      if(root1==NULL && root2==NULL)
-              return true;
-      if(root1==NULL || root2==NULL)
-                 return false;
+        bool isSameTree(TreeNode* p, TreeNode* q) {
+            
+            if(p==NULL && q==NULL) return true;
+            if(p==NULL && q!=NULL) return false;
+            if(p!=NULL && q==NULL) return false;
+            
+            if(p->val!=q->val)
+            {
+                    return false;
+            }
+               
+            return isSameTree(p->left,q->right) && isSameTree(p->right,q->left);
         
-      if(root1->val == root2->val)
-      {
-           return   isMirror(root1->left,root2->right) && isMirror(root1->right,root2->left); 
-      }
-         else
-                 return false;
-              
-         
-           
-   }
-
- bool isSymmetric(TreeNode* root) {
-        if(root==NULL) return true ;
-        return isMirror(root->left,root->right) ;
+    }
+    bool isSymmetric(TreeNode* root) {
+            
+            return isSameTree(root->left,root->right);
+            
+        
     }
 };
