4a52001c483f443c2d804800e97abb63350f5d52
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-06-08 14:18:49 +0530

    Update 1305. All Elements in Two Binary Search Trees.cpp

--- Leetcode Solution/Tree/Medium/1305. All Elements in Two Binary Search Trees.cpp
+++ Leetcode Solution/Tree/Medium/1305. All Elements in Two Binary Search Trees.cpp
@@ -5,32 +5,70 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+Solution 1:
+Time O(N+M) space(N+M)
+
+Approach :
+Find inorder traversal of both tree
+now make an answer vector and insert values of both tree using two pointer technique
+
+
+
+
 class Solution {
 public:
-         vector<int> ans;
-        void getval(TreeNode* node)
+        void inorder(TreeNode* r,vector<int> &a)
         {
-              if(!node) return ;
-                ans.push_back(node->val);
-        getval(node->left);
-                getval(node->right);
+                if(!r) return;
                 
-                
-                
+                inorder(r->left,a);
+                a.push_back(r->val);
+                inorder(r->right,a);
                 
         }
-        
-    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        
-             getval(root1);
-            getval(root2);
+    vector<int> getAllElements(TreeNode* r, TreeNode* rr) {
             
-            sort(ans.begin(),ans.end());
+            vector<int> ansr;
+            vector<int> ansrr;
+            vector<int> ans;
+            inorder(r,ansr);
+            inorder(rr,ansrr);
+            
+            int i=0;
+            int j=0;
+            int n=ansr.size();
+            int m=ansrr.size();
+            while(i<n && j<m)
+            {
+                    if(ansr[i]>ansrr[j])
+                    {
+                            ans.push_back(ansrr[j]);
+                            j++;
+                    }
+                    else
+                    {
+                            ans.push_back(ansr[i]);
+                            i++;
+                    }
+            }
+            
+            
+            while(i<n)
+            {
+                    ans.push_back(ansr[i++]);
+            }
+            while(j<m)
+            {
+                    ans.push_back(ansrr[j++]);
+            }
             
             
             return ans;
+        
     }
 };
