06c1ec4f9426d7e83cbd7c63cbf5183d060b2e19
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-06-10 13:33:27 +0530

    Update 37. Sudoku Solver.cpp

--- Leetcode Solution/BackTracking/Hard/37. Sudoku Solver.cpp
+++ Leetcode Solution/BackTracking/Hard/37. Sudoku Solver.cpp
@@ -1,4 +1,11 @@
 
+Time complexity: O(9^(n*n)). 
+For every unassigned index, there are 9 possible options so the time complexity is O(9^(n*n)).
+The time complexity remains the same but there will be some early pruning so the time taken will
+be much less than the naive algorithm but the upper bound time complexity remains the same.
+      
+Space Complexity: O(n*n). 
+To store the output array a matrix is needed.
 
 
 class Solution {
