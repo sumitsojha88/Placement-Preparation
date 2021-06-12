6cc0bd52449e32e7e970ea8197075904d0acd5bc
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-06-12 12:42:23 +0530

    Create 90. Subsets II.cpp

---
+++ Leetcode Solution/BackTracking/Medium/90. Subsets II.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+     void backtrack(vector<int>& nums,vector<vector<int>>& ans,vector<int>& curr,int i,int n)
+        {
+               ans.push_back(curr);
+                for(int j=i;j<n;j++)
+                {
+                        if(i!=j && nums[j-1]==nums[j])
+                        {
+                                continue;
+                        }
+                        
+                        curr.push_back(nums[j]);
+                        backtrack(nums,ans,curr,j+1,n);
+                        curr.pop_back();          
+                }        
+        }    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+            int n=nums.size();
+            sort(nums.begin(),nums.end());
+            vector<vector<int>> ans;
+            vector<int> curr;
+            backtrack(nums,ans,curr,0,n);
+            return ans;    
+    }
+};
