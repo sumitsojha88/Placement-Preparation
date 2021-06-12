d2e42aa82974be428814a951b69c3a878dd33cff
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-06-12 12:16:06 +0530

    Create 77. Combinations.cpp

---
+++ Leetcode Solution/BackTracking/Medium/77. Combinations.cpp
@@ -0,0 +1,29 @@
+
+
+class Solution {
+public:
+        void backtrack(vector<int>& curr,vector<vector<int>>& ans,int k,int i,int n)
+        {
+             if(curr.size()==k)
+             {
+                     ans.push_back(curr);
+                     return;
+             }
+                
+                for(int j=i;j<=n;j++)
+                {
+                        curr.push_back(j);
+                        backtrack(curr,ans,k,j+1,n);
+                        curr.pop_back();     
+                }
+            
+        }
+    vector<vector<int>> combine(int n, int k) {
+            vector<vector<int>> ans;
+            vector<int> curr;
+            
+            backtrack(curr,ans,k,1,n);
+            return ans;
+        
+    }
+};
